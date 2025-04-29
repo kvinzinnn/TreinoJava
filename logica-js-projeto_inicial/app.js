748085d86c7ea2a7479502417fe8740238a5fc94
Kevin Ferreira <kevin.2006.rf@gmail.com>
2025-04-29 13:30:45 -0300

    Treino JavaScript

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,7 @@
+alert('Boas Vindas ao jogo do número secreto'); 
+let numeroSecreto = 5;
+let chute = prompt('Escolha um número de 1 e 10');
+
+if (chute == numeroSecreto ) {
+    console.log('Isso aí! Você acertou porra (5)') ;
+}
